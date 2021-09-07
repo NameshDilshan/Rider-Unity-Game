f13b8e00a99cfdf3adb0ce2ce164cffd2aaedc6e
batman000001 <43162155+batman000001@users.noreply.github.com>
2021-09-07 22:34:47 +0530

    Init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    //same as update run by frame by frame, but run after update method and other updates
+    private void LateUpdate(){
+        Vector3 newPos = target.position + offset;
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    bool move = false;
+    bool isGrounded = false;
+
+    public Rigidbody2D rb;
+    public float speed = 20f;
+    public float rotationSpeed = 7f;
+
+
+    //update used for getting input
+    //called frame by frame. so, Quick
+    private void Update(){
+        if(Input.GetButtonDown("Fire1")){
+            Debug.Log("working down");
+            move = true;
+        }
+        if(Input.GetButtonUp("Fire1")){
+            move = false;
+        }
+    }
+
+    //fixedupdate used for controlling the car 
+    //called fixed amount of times.
+    private void FixedUpdate(){
+
+        if(move == true){
+            if(isGrounded){
+                rb.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f , ForceMode2D.Force);
+            }else{
+                rb.AddTorque(rotationSpeed * Time.fixedDeltaTime * 100f , ForceMode2D.Force);
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(){
+        isGrounded = true;
+    } 
+
+    private void OnCollisionExit2D(){
+        isGrounded = false;
+    }
+}
